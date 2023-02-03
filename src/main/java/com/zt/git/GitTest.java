ac1316152a6cac026afeec2dbc5838115165e295
zt <2916461184@qq.com>
2023-02-03 12:21:30 +0800

    first commit

---
+++ src/main/java/com/zt/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.zt.git;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.zt.git
+ * @Author: zt
+ * @CreateTime: 2023-02-03  12:16
+ * @Description:
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
